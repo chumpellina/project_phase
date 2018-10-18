16a637e1a276571077e6a7830f0cfccf65eaf687
Szabó Zsófia <szabozsofiaeva@gmail.com>
2018-10-18 19:04:55 +0200

    added finished weather app and some jinja

--- JSON/venv/weather.py
+++ JSON/venv/weather.py
@@ -1,34 +1,36 @@
-from flask import Flask, render_template, jsonify, json
+from flask import Flask, render_template, jsonify, json, url_for, request
 import json
 import pandas as pd
-import urllib3
 import requests
+from flask_wtf import FlaskForm
+from wtforms import StringField
 
 app = Flask(__name__)
 
-
-chosen_city= "Budapest"
 key = "b732f35a4c2771732be3cd3f9acd84b2"
-url = "https://api.openweathermap.org/data/2.5/weather?q=" +chosen_city+ ",hu&units=metric&appid=" + key
-
-
-response = requests.get(url)
-data = json.loads(response.content)
-
-temp = "the temperature in " + "chosen city is : %d" % (data["main"]["temp"])
-pressure = "the pressure in " + "chosen city is : %d" % (data["main"]["pressure"])
-hum = "the humidity in " + "chosen city is : %d" % (data["main"]["humidity"])
-temp_min = "the minimum temperature in " + "chosen city is : %d" % (data["main"]["temp_min"])
-temp_may = "the maximum temperature in " + "chosen city is : %d" % (data["main"]["temp_max"])
-
 
 @app.route("/", methods=["GET"])
 def home_page():
+
     return render_template("main.html")
 
-@app.route("/weather", methods=["GET"])
-def print_weather():
-    return render_template("weather.html", temp=(temp), pressure=(pressure), hum=(hum), temp_min=(temp_min), temp_max=(temp_max))
+@app.route("/weather", methods=["POST"])
+def redirect():
+    chosen_city = request.form["chosen_city"]
+    url = "https://api.openweathermap.org/data/2.5/weather?q=" + chosen_city + ",hu&units=metric&appid=" + key
+    response = requests.get(url)
+    data = json.loads(response.content)
+    temperature = data["main"]["temp"]
+    temp = "the temperature in %s is : %d" % (chosen_city, data["main"]["temp"])
+    pressure = "the pressure in %s is : %d" % (chosen_city, data["main"]["pressure"])
+    hum = "the humidity in %s is : %d" % (chosen_city, data["main"]["humidity"])
+    temp_min = "the minimum temperature in %s is : %d" % (chosen_city, data["main"]["temp_min"])
+    temp_max = "the maximum temperature in %s is : %d" % (chosen_city, data["main"]["temp_max"])
+    return render_template("weather.html", temperature = temperature, temp=temp, pressure=pressure, hum=hum, temp_min=temp_min, temp_max=temp_max)
+
+
+
+
 
 
 if __name__ == '__main__':
